8a75087782cf0dc5f7f1be43d6bad8b533f3f652
Zaid1024 <141013542+Zaid1024@users.noreply.github.com>
2024-05-30 11:43:50 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,12 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
-# Create data
-X = np.linspace(-5, 5, 100)
-Y = np.linspace(-5, 5, 100)
-X, Y = np.meshgrid(X, Y)
-Z = np.sin(np.sqrt(X**2 + Y**2))
 
 # 3D surface plot
 fig = plt.figure(figsize=(10, 8))
