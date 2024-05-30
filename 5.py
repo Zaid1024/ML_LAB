048bdfd8a252278a2f281faeebe4d9c3f663cd78
Zaid1024 <141013542+Zaid1024@users.noreply.github.com>
2024-05-30 11:24:42 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -12,50 +12,35 @@ class NaiveBayesClassifier:
         self.classes = np.unique(y)
         for c in self.classes:
             self.prior[c] = np.mean(y == c)
-
         for feature in X.columns:
-            self.conditional[feature] = {}
-            for c in self.classes:
-                feature_values = X[feature][y == c]
-                self.conditional[feature][c] = {'mean': np.mean(feature_values), 'std': np.std(feature_values)}
+            self.conditional[feature] = {
+                c: {'mean': np.mean(X[feature][y == c]), 'std': np.std(X[feature][y == c])} 
+                for c in self.classes
+            }
 
     def predict(self, X):
+        def _gaussian_pdf(x, mean, std):
+            exponent = np.exp(-((x - mean) ** 2) / (2 * std ** 2))
+            return (1 / (np.sqrt(2 * np.pi) * std)) * exponent
+
         y_pred = []
         for _, sample in X.iterrows():
-            probabilities = {}
-            for c in self.classes:
-                probabilities[c] = self.prior[c]
-                for feature in X.columns:
+            probabilities = {c: self.prior[c] for c in self.classes}
+            for feature in X.columns:
+                for c in self.classes:
                     mean = self.conditional[feature][c]['mean']
                     std = self.conditional[feature][c]['std']
-                    x = sample[feature]
-                    probabilities[c] *= self._gaussian_pdf(x, mean, std)
+                    probabilities[c] *= _gaussian_pdf(sample[feature], mean, std)
             y_pred.append(max(probabilities, key=probabilities.get))
         return y_pred
 
-    def _gaussian_pdf(self, x, mean, std):
-        exponent = np.exp(-((x - mean) ** 2) / (2 * std ** 2))
-        return (1 / (np.sqrt(2 * np.pi) * std)) * exponent
-
-# Load dataset
-df = pd.read_csv('./dm/titanic.csv')
-df = df[['Survived', 'Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
-
-# Fill missing values
-df['Age'] = df['Age'].fillna(df['Age'].median())
-df['Fare'] = df['Fare'].fillna(df['Fare'].median())
-df.fillna({'Embarked': df['Embarked'].mode()[0]}, inplace=True)
-
-# Map categorical 'Embarked' column to numerical values
+# Load and preprocess dataset
+df = pd.read_csv('./dm/titanic.csv')[['Survived', 'Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
+df.fillna({'Age': df['Age'].median(), 'Fare': df['Fare'].median(), 'Embarked': df['Embarked'].mode()[0]}, inplace=True)
 df['Embarked'] = df['Embarked'].map({'C': 0, 'Q': 1, 'S': 2})
 
 # Split dataset into training and testing sets
-train, test = train_test_split(df, test_size=0.2)
-
-X_train = train.drop('Survived', axis=1)
-y_train = train['Survived']
-X_test = test.drop('Survived', axis=1)
-y_test = test['Survived']
+X_train, X_test, y_train, y_test = train_test_split(df.drop('Survived', axis=1), df['Survived'], test_size=0.2)
 
 # Train Naive Bayes Classifier
 classifier = NaiveBayesClassifier()
@@ -63,8 +48,5 @@ classifier.fit(X_train, y_train)
 
 # Predict and evaluate the model
 y_pred = classifier.predict(X_test)
-
-cm = confusion_matrix(y_test, y_pred)
-print("Confusion Matrix:\n", cm)
-accuracy = np.mean(y_pred == y_test)
-print("Accuracy:", accuracy)
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+print("Accuracy:", np.mean(y_pred == y_test))
